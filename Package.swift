9eaeb093c3171a1238f2d05f3ff87fc7feeb5b56
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-31 15:14:04 +0800

    Add minimum platform to `macOS 11`

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "RHCacheStore",
+    platforms: [
+        .macOS(.v11) // Specify macOS 11 as the minimum platform
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
