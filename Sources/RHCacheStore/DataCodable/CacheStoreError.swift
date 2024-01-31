b771595b26142a960f471ecd0d848dcb737f8e6e
Reed Hsin <hooy123456@gapp.nthu.edu.tw>
2024-01-31 15:13:23 +0800

    Move `CacheStoreError`  and `RetriveCacheResult` to a shared level folder

---
+++ Sources/RHCacheStore/DataCodable/CacheStoreError.swift
@@ -0,0 +1,16 @@
+//
+//  CacheStoreError.swift
+//
+//
+//  Created by Chung Han Hsin on 2024/1/30.
+//
+
+import Foundation
+
+public enum CacheStoreError: Error {
+    case failureDeletion
+    case failureInsertion
+    case failureRetrival
+    case failureSaveCache
+    case failureLoadCache
+}
---
+++ Sources/RHCacheStore/DataCodable/RetriveCacheResult.swift
@@ -0,0 +1,13 @@
+//
+//  RetriveCacheResult.swift
+//
+//
+//  Created by Chung Han Hsin on 2024/1/30.
+//
+
+import Foundation
+
+public enum RetriveCacheResult {
+    case empty
+    case found(Any)
+}
